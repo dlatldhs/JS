5f3a00c08b0036dbb7d6d75746146f65f8bd800b
dlatldhs <sionzz1713@gmail.com>
2022-01-09 21:48:13 +0900

    JS variable type & undefined , null .... study

---
+++ app.js
@@ -0,0 +1,11 @@
+const a = 5;// const(상수) 변하지 않는 값이라는 뜻
+const b = 4;
+const veryLongVariableName = 'Im Si On';// 기본적으로 JS 변수는 이런 형태로 사용함
+
+let c = 'always'// 선언 하고도 나중에 값을 바꿀 수 있음 그게 let 임
+var d = 'var can error'
+let somethings;
+console.log(somethings);// undefined <-- 공간 O 값 X
+//console.log(a+b);
+//console.log(b-a);
+//console.log("hello"+veryLongVariableName);
