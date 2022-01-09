691fdb8a1884c8e54681b3b784ff17db03e8c17b
dlatldhs <sionzz1713@gmail.com>
2022-01-09 22:34:14 +0900

    Functions

--- app.js
+++ app.js
@@ -1,24 +1,14 @@
-// object 는 dictionary 와 비슷함
-const Latte = {
-    kcal : '210',
-    caffeine : "105mg",
-    price : 2000,
+/* 함수 선언 하는 방법
+function sayHi(argument){// argument 인수는 function 안에서만 존재
+    console.log('hello! '+argument);
+}
+sayHi('sion')*/
+
+
+const person = {
+    name : 'sion',
+    hi : function sayHi(argument){
+        console.log('hello '+argument+' good day to study!');
+    },
 };
-
-console.log(Latte);
-// 둘 다 같은 의미
-console.log("Latte.caffeine : "+Latte.caffeine);
-console.log("Latte['caffeine'] : " + Latte["caffeine"]);
-
-// const 라도 object 안에 있는 값은 업데이트가 가능함
-Latte['price'] = 2500;
-console.log(Latte);
-
-// Add in Object
-// objectname.property = "value"
-Latte.name = "Madagascar VanillaBean Latte";
-console.log(Latte);
-
-// 이런 것도 가능함
-Latte.price = Latte.price-500;
-console.log(Latte);
+person.hi('ZION')
