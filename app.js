5028000678911c65edd42e25a112bfb529b01479
dlatldhs <sionzz1713@gmail.com>
2022-01-09 22:17:14 +0900

    Js Object

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-const ary = ['one','two','thr','for']// 기본적으로 파이썬 리스트 형태를 띄고 있음
+// object 는 dictionary 와 비슷함
+const Latte = {
+    kcal : '210',
+    caffeine : "105mg",
+    price : 2000,
+};
 
-console.log(ary)
+console.log(Latte);
+// 둘 다 같은 의미
+console.log("Latte.caffeine : "+Latte.caffeine);
+console.log("Latte['caffeine'] : " + Latte["caffeine"]);
 
-// Get Item
-console.log(ary[3])
+// const 라도 object 안에 있는 값은 업데이트가 가능함
+Latte['price'] = 2500;
+console.log(Latte);
 
-// Add one more
-ary.push('last')
+// Add in Object
+// objectname.property = "value"
+Latte.name = "Madagascar VanillaBean Latte";
+console.log(Latte);
 
-console.log(ary)
+// 이런 것도 가능함
+Latte.price = Latte.price-500;
+console.log(Latte);
