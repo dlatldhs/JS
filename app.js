0564be2133a182697b514e29a8d89240c2bc1990
dlatldhs <sionzz1713@gmail.com>
2022-01-09 21:57:34 +0900

    Arrays

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const a = 5;// const(상수) 변하지 않는 값이라는 뜻
-const b = 4;
-const veryLongVariableName = 'Im Si On';// 기본적으로 JS 변수는 이런 형태로 사용함
+const ary = ['one','two','thr','for']// 기본적으로 파이썬 리스트 형태를 띄고 있음
 
-let c = 'always'// 선언 하고도 나중에 값을 바꿀 수 있음 그게 let 임
-var d = 'var can error'
-let somethings;
-console.log(somethings);// undefined <-- 공간 O 값 X
-//console.log(a+b);
-//console.log(b-a);
-//console.log("hello"+veryLongVariableName);
+console.log(ary)
+
+// Get Item
+console.log(ary[3])
+
+// Add one more
+ary.push('last')
+
+console.log(ary)
