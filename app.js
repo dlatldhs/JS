0b12bba524c899d5e2d6cbf1227b35b94cc9e138
dlatldhs <sionzz1713@gmail.com>
2022-01-10 21:39:20 +0900

    if/else 문과 연산자 등을 활용한 프로그램 구성

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
-// type
-// 코드 멈추게 함 
-const age = parseInt(prompt("How old are you?"));// <----- 이게 안끝나면 밑에 있는게 실행안됨
+const age = parseInt(prompt("How old are you?"));
 
-// type 보는거 -- > typeof variableName
-
-//console.log(typeof age,typeof parseInt(age));
-//console.log(age,parseInt(age));
-// NaN -> Not a Number 
-// type 변환 function string -> int
-//console.log(parseInt(age));
-//console.log(isNaN(age));// inNaN return boolean T or F
-// F -> number가 아니라는 조건에 충족안함 -> number!
-// T -> number가 아니라는 조건에 충족함 -> Not a number
-if(isNaN(age)){// condition is ture or false
-    // condition == ture
-    console.log('Please wirte a number');
+if(isNaN(age) || age < 0){
+    console.log('Please write a real positive number');
+}
+else if(19>age){
+    console.log('You are too young');
+}
+else if(age >= 19 && age <= 50){
+    console.log('You can drink');
+}
+else if(50 < age && age <= 80){
+    console.log('You should exercise');
 }
 else{
-    console.log('your age : '+age);
-    console.log('Have some good day!')
+    console.log('You can do whatever you want.');
 }
