8b43abb680e579e5409ed771685fad5df8bb335c
dlatldhs <sionzz1713@gmail.com>
2022-01-10 10:08:45 +0900

    응용복습

--- app.js
+++ app.js
@@ -3,12 +3,25 @@ function sayHi(argument){// argument 인수는 function 안에서만 존재
     console.log('hello! '+argument);
 }
 sayHi('sion')*/
-
-
-const person = {
-    name : 'sion',
-    hi : function sayHi(argument){
-        console.log('hello '+argument+' good day to study!');
+// const person = {
+//     name : 'sion',
+//     hi : function sayHi(argument){
+//         console.log('hello '+argument+' good day to study!');
+//     },
+// };
+// person.hi('ZION')
+const calculator ={
+    add : function(a,b){
+        console.log(a+b);
+    },
+    m : function(a,b){
+        console.log(a-b);
+    },
+    wprhq : function(a,b){
+        console.log(a*b);
     },
 };
-person.hi('ZION')
+const a = 5, b = 4;
+calculator.add(a,b);
+calculator.m(a,b);
+calculator.wprhq(a,a);
