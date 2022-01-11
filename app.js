ba2e2190ebb24b69c4439bbde452a7218c547aa4
dlatldhs <sionzz1713@gmail.com>
2022-01-11 21:38:31 +0900

    functions practice

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
-// const hellos = document.getElementsByClassName('hello');
-// console.log(hellos);
-const t = document.querySelector('.hello h1:first-child');// class = 'hello' in first h1 tag 
+const t = document.querySelector('div.hello:first-child h1');// class = 'hello' in first h1 tag 
+function clickk(){
+    if(t.style.color === 'blue'){
+        t.style.color='black';
+    }
+    else{
+        t.style.color='blue';
+    }
+}
+function handleMouseEnter(){
+    //console.log('mouse is here!');
+    t.innerHTML = 'mouse Is Here!';
+}
+
+function handleMouseLeave(){
+    //console.log('mouse is out!');
+    t.innerHTML = 'mouse Is Out!';
+}
 // querySelectorAll <-- 조건에 만족하는 거 다 가져옴
-t.innerHTML = 'hello js!';
+//t.innerHTML = 'hello js!';
+//console.log(t);
+//console.dir(t);// object를 보여줌
+//t.style.color='blue';// t object in style property in color 대충 이런 느낌 ㅇㅇ 
+t.addEventListener('click',clickk);// event listen
+t.addEventListener('mouseenter',handleMouseEnter);
+t.addEventListener('mouseleave',handleMouseLeave);
