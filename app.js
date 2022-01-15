93ac9104814509c1d0874fce63ca2ccaf243d858
dlatldhs <sionzz1713@gmail.com>
2022-01-15 21:20:20 +0900

    toggle

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const h1 = document.querySelector('div.hello:first-child h1');// class = 'hello' in first h1 tag 
 
-const classNames = "active";
+const clickClassName = "active";
 function handleTitleClick(){
-    if(h1.className === classNames){
-        h1.className = "";
-    }
-    else{
-        h1.className = classNames;
-    }
+    //if(h1.classList.contains(clickClassName)){
+        //h1.classList.remove(clickClassName);
+    h1.classList.toggle('active');// h1에 active class가 있는지 확인해서 있음 제거 없음 넣음 
+    //}
+    //else{
+        
+        //h1.classList.add(clickClassName);// className 말고 classList 로 하게 되면 html 에 class가 변경안되고 추가됨
+    //}
 }
 
 h1.addEventListener("click",handleTitleClick);
