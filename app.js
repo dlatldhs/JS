9ba556d9e6fbe26a8cbd482cbab52e5c50642365
dlatldhs <sionzz1713@gmail.com>
2022-01-16 01:20:21 +0900

    localStorage로 UserInfo & login 기능 좀 더 좋게 만들 어 보 기

--- app.js
+++ app.js
@@ -2,15 +2,35 @@ const loginInput = document.querySelector('#login-form input');
 const loginForm = document.querySelector('#login-form');
 const greeting = document.querySelector('#greeting');
 
+// this is Strings
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event){
-    event.preventDefault();
-    const userName = loginInput.value;
+    event.preventDefault();// 안하게 해줌
+    const username = loginInput.value;
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    greeting.innerHTML = "Hello " + userName;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
-
+    localStorage.setItem(USERNAME_KEY,username);
+    paintGreetings(username);
 }
+/*
+localStorage.setItem("username","ZION") add
+localStorage.removeItem("username") remove 
+*/
+function paintGreetings(username){
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    greeting.innerHTML = `Hello ${username} Good Day!`;
+}
+const localStorageUserNamed = localStorage.getItem(USERNAME_KEY);
 
-loginForm.addEventListener('submit',onLoginSubmit);
+if(localStorageUserNamed === null){
+    // show login form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener('submit',onLoginSubmit);
+}
+else{
+    paintGreetings(localStorageUserNamed);
+    // show main form 
+    // greeting.classList.remove(HIDDEN_CLASSNAME);
+    // greeting.innerHTML = `Hello ${localStorageUserNamed} Good Day!`;
+}
