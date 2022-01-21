3dd80cfc875692b6f6a75927d50039c595b3173a
dlatldhs <sionzz1713@gmail.com>
2022-01-21 18:25:47 +0900

    css 작업 중 거의 다 했는데 배경이 안됨;; 처음부터 다시 해야할 수도

--- JS/background.js
+++ JS/background.js
@@ -1,11 +1,7 @@
-const images = [
-    "0.png","1.png","2.png","3.png","4.png","5.png","6.png"
-];
-const choiceRandomImage = images[Math.floor(Math.random()*images.length)];
+const images = ["0.png","1.png","2.png","3.png","4.png","5.png","6.png"];
 
+const choiceRandomImage = images[Math.floor(Math.random()*images.length)];
 const IMA = document.createElement('img');
 IMA.src = `CSS/img/${choiceRandomImage}`;
-
-console.log(IMA);
 
 document.body.appendChild(IMA);
