491a7aaee65b2733a94e072d063089339c09c41f
임시온 <80656700+dlatldhs@users.noreply.github.com>
2022-04-25 21:24:12 +0900

    Add files via upload

---
+++ homework/6/src/source/My_Information.js
@@ -0,0 +1,13 @@
+export default function MyInformation() {
+  return (
+    <div>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwuGe04XL-8CHPZ15hEp8OGoE1pkhb_XX6hg&usqp=CAU" />
+      <div class="text">
+        <h1 id="my_name">I'm Si ON</h1>
+        <h3 id="job">Back-End Developer</h3>
+        <h5 id="school_name">BSSM</h5>
+        <button>✉ Email</button>
+      </div>
+    </div>
+  );
+}
