c31914cc57d92e7e9691e9f45347cd473ecf9cd6
임시온 <80656700+dlatldhs@users.noreply.github.com>
2022-05-09 22:01:20 +0900

    Add files via upload

---
+++ my_info_card/src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import MyInformation from "./source/My_Information.js";
+import Mainbody from "./source/Main_body.js";
+
+export default function App() {
+  return (
+    <div className="App">
+      <MyInformation />
+      <Mainbody />
+    </div>
+  );
+}
---
+++ my_info_card/src/source/Main_body.js
@@ -0,0 +1,16 @@
+export default function MyInformation() {
+  return (
+    <div id="main_body">
+      <h1>About</h1>
+      <p>
+        Hello my name is I'm Si On. I want to be a server developer. I'm
+        currently attending BSSM and will try to become a better developer.
+      </p>
+      <h1>Habit</h1>
+      <p>
+        I like listening to music. Among them, I especially like hip hop genre.
+        My favorite rapper is Lil Boi.
+      </p>
+    </div>
+  );
+}
